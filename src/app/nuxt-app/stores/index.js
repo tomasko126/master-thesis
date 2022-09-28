bffde051535799cca88daedd072cff77bcfb286b
Tomáš Taro <tomasko126@gmail.com>
2022-09-28 11:17:59 +0200

    Introduce global Pinia store

--- src/app/nuxt-app/stores/index.js
+++ src/app/nuxt-app/stores/index.js
@@ -13,13 +13,17 @@ export const useGlobalStore = defineStore({
     state: () => {
         return {
             init: false,
-            elemActivated: false,
+            imageContainers: [],
+            mainImageContainer: null,
+            imageIds: [],
         };
     },
     getters: {
     },
     actions: {
         async initLibraries() {
+            // todo: do not store the reference to cornerstone in the |window| object
+            window.cornerstone = cornerstone
             if (this.init) {
                 return;
             }
@@ -62,42 +66,91 @@ export const useGlobalStore = defineStore({
             this.init = true;
         },
         /**
-         * Load images from given |files| and show the first one as a content of the |element|
-         * @param {File[]} files
-         * @param {HTMLElement} element
+         * Load images from given |files| and show the first in a main window
+         * @param {File[]} imageFiles
+         * @param {HTMLElement} imageContainer
          * @returns {Promise<void>}
          */
-        async loadImagesFromFiles(files, element) {
-            if (!this.elemActivated) {
-                cornerstone.enable(element);
-                this.elemActivated = true;
-            }
+        async loadImagesFromFiles(imageFiles, imageContainer) {
+            // First, we need to register main window as an image container
+            this.registerImageContainer(imageContainer, true);
 
+            // Retrieve image IDs used later for referencing images
             const imageIds = [];
-            for (const file of files) {
-                imageIds.push(cornerstoneWADOImageLoader.wadouri.fileManager.add(file));
+            for (const imageFile of imageFiles) {
+                imageIds.push(cornerstoneWADOImageLoader.wadouri.fileManager.add(imageFile));
             }
+            this.imageIds = imageIds;
 
             try {
-                const image = await cornerstone.loadImage(imageIds[0]);
                 const stack = {
                     currentImageIdIndex: 0,
-                    imageIds,
+                    imageIds: this.imageIds,
                 };
-                cornerstoneTools.addStackStateManager(element, ['stack']);
-                cornerstoneTools.addToolState(element, 'stack', stack);
-                cornerstone.displayImage(element, image);
+                cornerstoneTools.addStackStateManager(imageContainer, ['stack']);
+                cornerstoneTools.addToolState(imageContainer, 'stack', stack);
 
-                // todo: move this to another place
-                const FreehandRoiTool = cornerstoneTools.FreehandRoiTool;
-                cornerstoneTools.addTool(FreehandRoiTool);
-                cornerstoneTools.setToolActive('FreehandRoi', { mouseButtonMask: 1 });
+                await this.displayImageInElement(imageContainer, this.imageIds[0]);
 
-                const StackScrollMouseWheelTool = cornerstoneTools.StackScrollMouseWheelTool;
-                cornerstoneTools.addTool(StackScrollMouseWheelTool);
-                cornerstoneTools.setToolActive('StackScrollMouseWheel', {});
+                // todo: move tools functionality to another place
+                // const FreehandRoiTool = cornerstoneTools.FreehandRoiTool;
+                // cornerstoneTools.addTool(FreehandRoiTool);
+                // cornerstoneTools.setToolActive('FreehandRoi', { mouseButtonMask: 1 });
+
+                // const StackScrollMouseWheelTool = cornerstoneTools.StackScrollMouseWheelTool;
+                // cornerstoneTools.addTool(StackScrollMouseWheelTool);
+                // cornerstoneTools.setToolActive('StackScrollMouseWheel', {});
             } catch (e) {
                 console.error(e);
+            }
+        },
+        /**
+         * Display image in a given element
+         * @param {HTMLElement} imageContainer
+         * @param {string} imageId
+         * @returns {Promise<void>}
+         */
+        async displayImageInElement(imageContainer, imageId) {
+            this.registerImageContainer(imageContainer);
+            const image = await cornerstone.loadImage(imageId);
+            cornerstone.displayImage(imageContainer, image);
+        },
+        /**
+         * Display image in main window
+         * @param {string} imageId
+         * @returns {Promise<void>}
+         */
+        async displayImageInMainWindow(imageId) {
+            const image = await cornerstone.loadImage(imageId);
+            cornerstone.displayImage(this.mainImageContainer, image);
+        },
+        /**
+         * Register image container
+         * @param {HTMLElement} imageContainer
+         * @param {boolean} isMainImageContainer
+         */
+        registerImageContainer(imageContainer, isMainImageContainer = false) {
+            if (!this.imageContainers.includes(imageContainer)) {
+                cornerstone.enable(imageContainer);
+                this.imageContainers.push(imageContainer);
+                if (isMainImageContainer) {
+                    this.mainImageContainer = imageContainer;
+                }
+            }
+        },
+        /**
+         * Unregister image container
+         * @param {HTMLElement} imageContainer
+         * @param {boolean} isMainImageContainer
+         */
+        unregisterImageContainer(imageContainer, isMainImageContainer = false) {
+            const imageFileIdx = this.imageContainers.indexOf(imageContainer);
+            if (imageFileIdx > -1) {
+                cornerstone.disable(imageContainer);
+                this.imageContainers.splice(imageFileIdx, 1);
+                if (isMainImageContainer) {
+                    this.mainImageContainer = null;
+                }
             }
         },
     },
