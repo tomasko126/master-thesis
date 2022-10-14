20e120660553276f7b1f713e3830f133130f445e
Tomáš Taro <tomasko126@gmail.com>
2022-10-14 12:58:27 +0200

    Refactor animation section business logic

--- src/app/nuxt-app/components/right-panel/tools/AnimationSection.vue
+++ src/app/nuxt-app/components/right-panel/tools/AnimationSection.vue
@@ -5,36 +5,33 @@
         <label for="animation-speed">Speed:</label>
         <input
           id="animation-speed"
-          ref="animation-speed"
+          v-model="speed"
           type="range"
           min="50"
           max="250"
           step="10"
-          @change="onRangeInput"
         >
       </div>
       <div class="input-wrapper">
         <label for="from-image-idx">From image:</label>
         <input
           id="from-image-idx"
-          ref="from-image-idx"
+          v-model="fromIdx"
           class="idx-inputs"
           type="number"
           :min="store.imageIds ? 1 : 0"
           :max="store.imageIds ? store.imageIds.length : 1"
-          @input="store.animation.fromIdx = $event.target.value - 1"
         >
       </div>
       <div class="input-wrapper">
         <label for="to-image-idx">To image:</label>
         <input
           id="to-image-idx"
-          ref="to-image-idx"
+          v-model="toIdx"
           class="idx-inputs"
           type="number"
           :min="store.imageIds ? 1 : 0"
           :max="store.imageIds ? store.imageIds.length : 1"
-          @input="store.animation.toIdx = $event.target.value - 1"
         >
       </div>
     </template>
@@ -49,36 +46,46 @@ export default {
     const store = useGlobalStore();
     return { store };
   },
-  watch: {
-    imageIds(newValue) {
-      this.$refs['from-image-idx'].value = '1';
-      this.$refs['to-image-idx'].value = newValue.length;
-    },
+  data() {
+    return {
+      fromIdx: this.store.animation.fromIdx,
+      toIdx: this.store.animation.toIdx,
+      speed: this.convertSpeedToInputValue(this.store.animation.speed),
+    };
   },
-  mounted() {
-    // Set animation speed slider value
-    this.$refs['animation-speed'].value = this.normalizeSpeed(this.store.animation.speed);
+  watch: {
+    'store.imageIds'(newValue) {
+      this.fromIdx = 1;
+      this.toIdx = newValue.length;
+    },
+    fromIdx(newValue) {
+      this.store.animation.fromIdx = newValue - 1;
+    },
+    toIdx(newValue) {
+      this.store.animation.toIdx = newValue - 1;
+    },
+    speed(newValue) {
+      this.store.animation.speed = this.convertSpeedToInputValue(newValue);
+    },
   },
   methods: {
-    normalizeSpeed(desiredSpeed) {
+    convertSpeedToInputValue(speed) {
       const MAX_SPEED_PLUS_MIN_SPEED = 300;
-      return MAX_SPEED_PLUS_MIN_SPEED - desiredSpeed;
-    },
-    onRangeInput(event) {
-      this.store.animation.speed = this.normalizeSpeed(event.target.value);
+      return MAX_SPEED_PLUS_MIN_SPEED - speed;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.idx-inputs {
-  width: 100px;
-}
 .input-wrapper {
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 10px 0;
+
+  .idx-inputs {
+    width: 100px;
+  }
 }
 </style>
