864ef02453c501207a49654ce0eb8cdecc692cf5
Tomáš Taro <tomasko126@gmail.com>
2023-01-31 00:30:39 +0100

    Update nuxt config file with cornerstone scripts and its dependencies

--- src/app/nuxt-app/nuxt.config.ts
+++ src/app/nuxt-app/nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  buildModules: ['@pinia/nuxt'],
+  app: {
+    head: {
+      script: [
+        { src: 'https://cdn.jsdelivr.net/npm/cornerstone-math@0.1.10/dist/cornerstoneMath.js' },
+        { src: 'https://cdn.jsdelivr.net/npm/cornerstone-core@2.6.1/dist/cornerstone.js' },
+        { src: 'http://localhost:9000/dist/cornerstoneTools.js' },
+        { src: 'https://cdn.jsdelivr.net/npm/cornerstone-wado-image-loader@4.8.0/dist/cornerstoneWADOImageLoader.bundle.min.js' },
+        { src: 'https://cdn.jsdelivr.net/npm/dicom-parser@1.8.20/dist/dicomParser.js' },
+        { src: 'https://cdn.jsdelivr.net/npm/hammerjs@2.0.8/hammer.js' },
+      ],
+    },
+  },
+  css: [
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
   modules: [
     '@pinia/nuxt',
   ],
-  optimizeDeps: {
-    exclude: ['cornerstone-tools']
-  },
   plugins: [
     {
       src: '~/plugins/oruga.js',
