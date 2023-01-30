894f31ac15a5df8a8d6c3a2fdb0094a3adb6fb38
Tomáš Taro <tomasko126@gmail.com>
2023-01-31 00:31:44 +0100

    Setup FontAwesome in the project

--- src/app/nuxt-app/plugins/oruga.js
+++ src/app/nuxt-app/plugins/oruga.js
@@ -3,6 +3,21 @@ import Oruga from '@oruga-ui/oruga-next';
 import { bulmaConfig } from '@oruga-ui/theme-bulma';
 import '@oruga-ui/theme-bulma/dist/bulma.css';
 
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+// This is important, we are going to let Nuxt worry about the CSS
+config.autoAddCss = false;
+
+library.add(far, fas);
+
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(Oruga, bulmaConfig);
+    nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
+    nuxtApp.vueApp.use(Oruga, {
+        ...bulmaConfig,
+        iconComponent: 'font-awesome-icon',
+        iconPack: 'fas',
+    });
 });
