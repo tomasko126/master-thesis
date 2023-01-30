052e39c53184bad2b3da306b68305936aa7fe80f
Tomáš Taro <tomasko126@gmail.com>
2023-01-31 00:34:38 +0100

    Remove duplicated methods
    They have been moved to components

--- src/app/nuxt-app/stores/index.js
+++ src/app/nuxt-app/stores/index.js
@@ -1,14 +1,15 @@
 import { defineStore } from 'pinia';
 
-import cornerstone from 'cornerstone-core';
-import cornerstoneMath from 'cornerstone-math';
-import 'cornerstone-tools';
-import Hammer from 'hammerjs';
+//import cornerstone from "cornerstone-core";
+//window.cornerstone = cornerstone; // todo: remove once we do not need it
+// import cornerstoneMath from 'cornerstone-math';
+// import 'cornerstone-tools';
+// import Hammer from 'hammerjs';
 
-import dicomParser from 'dicom-parser';
-import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+// import dicomParser from 'dicom-parser';
+// import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 
-import { sleep } from '~/functions/utils';
+/* global cornerstone, cornerstoneTools, cornerstoneWADOImageLoader*/
 
 export const useGlobalStore = defineStore({
     id: 'globalStore',
@@ -20,57 +21,19 @@ export const useGlobalStore = defineStore({
                 toIdx: 0, // image idx
             },
             isLoopingImages: false,
-            imageContainers: [],
             mainImageContainer: null,
             imageIds: [],
-            lastImageId: null,
+            shownImageId: null,
             toolNames: [],
         };
     },
     actions: {
-        async initLibraries() {
-            // Setup all required cornerstone-tools dependencies
-            cornerstoneTools.external.cornerstone = cornerstone;
-            cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
-            cornerstoneTools.external.Hammer = Hammer;
-            cornerstoneTools.init({
-                mouseEnabled: true,
-                showSVGCursors: true,
-            });
-
-            // Setup all required cornerstone-wado-image-loader dependencies
-            cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
-            cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
-
-            cornerstoneWADOImageLoader.configure({
-                useWebWorkers: true,
-                decodeConfig: {
-                    convertFloatPixelDataToInt: false,
-                },
-            });
-
-            const config = {
-                maxWebWorkers: navigator.hardwareConcurrency || 1,
-                startWebWorkersOnDemand: false,
-                taskConfiguration: {
-                    decodeTask: {
-                        initializeCodecsOnStartup: true,
-                        strict: false,
-                    },
-                },
-            };
-
-            cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
-        },
         /**
-         * Load images from given |files| and show the first in a main window
+         * Load images from given |files| and show the first one in a main window
          * @param {File[]} imageFiles
          * @returns {Promise<void>}
          */
         async loadImagesFromFiles(imageFiles) {
-            // Register main image container
-            this.registerImageContainer(document.getElementById('dicom-image'), true);
-
             // Retrieve image IDs used for referencing images
             const imageIds = [];
             for (const imageFile of imageFiles) {
@@ -83,141 +46,67 @@ export const useGlobalStore = defineStore({
                 currentImageIdIndex: 0,
                 imageIds: this.imageIds,
             };
+
+            // We have to reset enabled element
+            cornerstone.disable(this.mainImageContainer);
+            cornerstone.enable(this.mainImageContainer);
+
             cornerstoneTools.addStackStateManager(this.mainImageContainer, ['stack']);
             cornerstoneTools.addToolState(this.mainImageContainer, 'stack', stack);
 
-            // Display first image in main window
-            await this.displayImageInMainWindow(this.imageIds[0]);
-
             // Set default animation settings
-            this.setDefaultAnimationSettings();
-        },
-        /**
-         * Display image in a given element
-         * @param {HTMLElement} imageContainer
-         * @param {string} imageId
-         * @returns {Promise<void>}
-         */
-        async displayImageInElement(imageContainer, imageId) {
-            const image = await cornerstone.loadAndCacheImage(imageId);
-            cornerstone.displayImage(imageContainer, image);
-            this.lastImageId = imageId;
-        },
-        /**
-         * Display image in main window
-         * @param {string} imageId
-         * @returns {Promise<void>}
-         */
-        async displayImageInMainWindow(imageId) {
-            const image = await cornerstone.loadAndCacheImage(imageId);
-            cornerstone.displayImage(this.mainImageContainer, image);
-            this.lastImageId = imageId;
+            this.animation = {
+                speed: 50,
+                fromIdx: 0,
+                toIdx: Math.max( this.imageIds.length - 1, 0),
+            }
+
+            // Display first image in main window
+            this.shownImageId = this.imageIds[0] ?? null;
         },
         /**
          * Register image container
-         * @param {HTMLElement} imageContainer
+         * @param {HTMLElement} element
          * @param {boolean} isMainImageContainer
          */
-        registerImageContainer(imageContainer, isMainImageContainer = false) {
-            if (!this.imageContainers.includes(imageContainer)) {
-                cornerstone.enable(imageContainer);
-                this.imageContainers.push(imageContainer);
-                if (isMainImageContainer) {
-                    this.mainImageContainer = imageContainer;
-                }
+        registerImageContainer(element, isMainImageContainer = false) {
+            cornerstone.enable(element);
+            if (isMainImageContainer) {
+                this.mainImageContainer = element;
             }
         },
         /**
          * Unregister image container
-         * @param {HTMLElement} imageContainer
+         * @param {HTMLElement} element
          * @param {boolean} isMainImageContainer
          */
-        unregisterImageContainer(imageContainer, isMainImageContainer = false) {
-            const imageFileIdx = this.imageContainers.indexOf(imageContainer);
-            if (imageFileIdx > -1) {
-                cornerstone.disable(imageContainer);
-                this.imageContainers.splice(imageFileIdx, 1);
-                if (isMainImageContainer) {
-                    this.mainImageContainer = null;
-                }
+        unregisterImageContainer(element, isMainImageContainer = false) {
+            cornerstone.disable(element);
+            if (isMainImageContainer) {
+                this.mainImageContainer = null;
             }
         },
-        addTool(toolName) {
-          this.toolNames.push(toolName);
-        },
-        /**
-         * @param {string} toolName
-         * @param {Object} toolOptions
-         */
         activateTool(toolName, toolOptions = {}) {
             // todo: deactivate another tools
-            cornerstoneTools.setToolActive(toolName, toolOptions);
+            cornerstoneTools.setToolActiveForElement(this.mainImageContainer, toolName, toolOptions);
         },
         registerTool(toolName) {
             const tool = cornerstoneTools[`${toolName}`];
-            if (!tool) {
-                throw new Error(`Tool ${toolName} does not exist!`);
-            }
-            cornerstoneTools.addTool(tool);
+            cornerstoneTools.addToolForElement(this.mainImageContainer, tool);
+        },
+        unregisterTool(toolName) {
+            const tool = cornerstoneTools[`${toolName}`];
+            cornerstoneTools.removeTool(tool);
         },
         registerAllTools() {
             for (const toolName of this.toolNames) {
                 this.registerTool(toolName);
             }
         },
-        /**
-         * Start looping selected images in the main window
-         * todo: when idx are updated, it is not reflected in the video sequence, bug when max idx is less than min
-         * @returns {Promise<void>}
-         */
-        async startLoopingImages() {
-            this.isLoopingImages = true;
-
-            const leftImageIds = this.imageIds.slice(this.imageIds.indexOf(this.lastImageId), this.animation.toIdx + 1);
-            for (const imageId of leftImageIds) {
-                if (!this.isLoopingImages) {
-                    return;
-                }
-                await this.displayImageInMainWindow(imageId);
-                if (!this.isLoopingImages) {
-                    return;
-                }
-                await sleep(this.animation.speed);
+        unregisterAllTools() {
+            for (const toolName of this.toolNames) {
+                this.unregisterTool(toolName);
             }
-
-            const allSelectedImages = this.imageIds.slice(this.animation.fromIdx, this.animation.toIdx + 1);
-            while (this.isLoopingImages) {
-                for (const imageId of allSelectedImages) {
-                    if (!this.isLoopingImages) {
-                        return;
-                    }
-                    await this.displayImageInMainWindow(imageId);
-                    if (!this.isLoopingImages) {
-                        return;
-                    }
-                    await sleep(this.animation.speed);
-                }
-            }
-        },
-        stopLoopingImages() {
-            this.isLoopingImages = false;
-        },
-        setDefaultAnimationSettings() {
-            this.animation = {
-                speed: 50,
-                fromIdx: 0,
-                toIdx: this.imageIds.length - 1,
-            }
-        },
-        updateBrightness(newValue) {
-            const viewport = cornerstone.getViewport(this.mainImageContainer);
-            viewport.voi.windowCenter = newValue;
-            cornerstone.setViewport(this.mainImageContainer, viewport);
-        },
-        updateContrast(newValue) {
-            const viewport = cornerstone.getViewport(this.mainImageContainer);
-            viewport.voi.windowWidth = newValue;
-            cornerstone.setViewport(this.mainImageContainer, viewport);
         },
     },
 });
