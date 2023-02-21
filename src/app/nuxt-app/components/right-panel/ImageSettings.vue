221575c0b54817161305dcbfeb97f34396f03c5a
Tomáš Taro <tomasko126@gmail.com>
2023-02-21 12:22:13 +0100

    Add frame number to image settings

--- src/app/nuxt-app/components/right-panel/ImageSettings.vue
+++ src/app/nuxt-app/components/right-panel/ImageSettings.vue
@@ -1,6 +1,19 @@
 <template>
   <GeneralTabContent title="Image">
     <template #content>
+      <GeneralTabSection label-text="Frame:">
+        <template #content>
+          <va-counter
+              v-model="counter"
+              class="counter"
+              :disabled="!store.imageIds.length || store.isLoopingImages"
+              :min="1"
+              :max="store.imageIds.length"
+              width="100px"
+              @update:model-value="onFrameNumberChange"
+          />
+        </template>
+      </GeneralTabSection>
       <GeneralTabSection label-text="Brightness:">
         <template #content>
           <div class="brightness-settings">
@@ -9,18 +22,14 @@
               :disabled="isButtonDisabled"
               @click="onBrightnessChange(10)"
             >
-              <template #default>
-                <font-awesome-icon icon="fa-solid fa-plus" />
-              </template>
+              <font-awesome-icon icon="fa-solid fa-plus" />
             </va-button>
             <va-button
               size="small"
               :disabled="isButtonDisabled"
               @click="onBrightnessChange(-10)"
             >
-              <template #default>
-                <font-awesome-icon icon="fa-solid fa-minus" />
-              </template>
+              <font-awesome-icon icon="fa-solid fa-minus" />
             </va-button>
           </div>
         </template>
@@ -33,18 +42,14 @@
               :disabled="isButtonDisabled"
               @click="onContrastChange(-10)"
             >
-              <template #default>
-                <font-awesome-icon icon="fa-solid fa-plus" />
-              </template>
+              <font-awesome-icon icon="fa-solid fa-plus" />
             </va-button>
             <va-button
               size="small"
               :disabled="isButtonDisabled"
               @click="onContrastChange(10)"
             >
-              <template #default>
-                <font-awesome-icon icon="fa-solid fa-minus" />
-              </template>
+              <font-awesome-icon icon="fa-solid fa-minus" />
             </va-button>
           </div>
         </template>
@@ -67,6 +72,18 @@ export default {
     const store = useGlobalStore();
     return { store };
   },
+  data() {
+    return {
+      counter: 1,
+    };
+  },
+  watch: {
+    'store.shownImageId': {
+      handler() {
+        this.counter = this.store.imageIds.indexOf(this.store.shownImageId) + 1;
+      },
+    },
+  },
   computed: {
     isButtonDisabled() {
       return this.store.imageIds.length === 0;
@@ -83,6 +100,14 @@ export default {
       viewport.voi.windowWidth -= scalar;
       cornerstone.setViewport(this.store.mainImageContainer, viewport);
     },
+    onFrameNumberChange(idx) {
+      const number = parseInt(idx) - 1;
+      if (isNaN(number) || number < 0 || number > this.store.imageIds.length) {
+        return;
+      }
+
+      this.store.displayImageInElement(this.store.mainImageContainer, this.store.imageIds[number]);
+    },
   },
 };
 </script>
@@ -93,5 +118,8 @@ export default {
     margin: 0 2.5px 0 2.5px;
     width: 35px;
   }
+}
+.counter {
+  max-width: 140px;
 }
 </style>
