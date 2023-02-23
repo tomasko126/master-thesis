4beac92e814f32290bfe515e389092d97ab293b0
Tomáš Taro <tomasko126@gmail.com>
2023-02-23 11:50:19 +0100

    Change global font's color

--- src/app/nuxt-app/pages/index.vue
+++ src/app/nuxt-app/pages/index.vue
@@ -6,7 +6,7 @@
 body {
   margin: 0;
   background-color: #121d2f;
-  color: rgba(255, 255, 255, 0.75);
+  color: rgba(255, 255, 255, 0.8);
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Helvetica Neue", sans-serif;
   font-weight: bold;
 }
