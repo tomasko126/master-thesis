8248c2ac7442191d7dcbc7929c262425e546cc78
Tomáš Taro <tomasko126@gmail.com>
2023-03-13 10:11:41 +0100

    Fix store indentation

--- src/app/nuxt-app/stores/index.ts
+++ src/app/nuxt-app/stores/index.ts
@@ -3,34 +3,34 @@ import GridState from '~/functions/GridState';
 import DicomHeaderParser from '~/functions/DicomHeaderParser';
 
 export const useGlobalStore = defineStore({
-    id: 'globalStore',
-    state: () => {
-        return {
-            algorithm: {
-                curvature: 0.0015,
-                force: 0.5,
-                stopTime: 0.150,
-            } as AlgorithmSettings,
-            animation: {
-                speed: 30, // in fps
-                fromIdx: 0, // image idx
-                toIdx: 0, // image idx
-            } as AnimationSettings,
-            isLoopingImages: false as boolean,
-            mainImageContainer: null as HTMLElement|null,
-            imageIds: [] as string[],
-            shownImageId: null as string|null,
-            activeTool: null as string|null,
-            tools: [] as string[],
-            dicomHeaderParser: null as DicomHeaderParser|null,
-            gridState: null as GridState|null,
-        };
+  id: 'globalStore',
+  state: () => {
+    return {
+      algorithm: {
+        curvature: 0.0015,
+        force: 0.5,
+        stopTime: 0.150,
+      } as AlgorithmSettings,
+      animation: {
+        speed: 30, // in fps
+        fromIdx: 0, // image idx
+        toIdx: 0, // image idx
+      } as AnimationSettings,
+      isLoopingImages: false as boolean,
+      mainImageContainer: null as HTMLElement|null,
+      imageIds: [] as string[],
+      shownImageId: null as string|null,
+      activeTool: null as string|null,
+      tools: [] as string[],
+      dicomHeaderParser: null as DicomHeaderParser|null,
+      gridState: null as GridState|null,
+    };
+  },
+  getters: {
+    hasImageDefinedGrid(state) {
+      return state.gridState?.hasGrid() ?? false;
     },
-    getters: {
-        hasImageDefinedGrid(state) {
-            return state.gridState?.hasGrid() ?? false;
-        },
-    },
+  },
 });
 
 interface AlgorithmSettings {
