ff10d2cb788ced39e4c375cf9e3bb55792043537
Tomáš Taro <tomasko126@gmail.com>
2023-04-21 00:07:21 +0200

    API communication refinements (#31)
    * API communication refinements
    
    * Update dependencies
    
    * Update package lock file
    
    * Change imports
    
    * Downgrade vue-tsc

--- app/components/left-panel/GridAlgorithmSettings.vue
+++ app/components/left-panel/GridAlgorithmSettings.vue
@@ -72,6 +72,7 @@ import Communication from '../../functions/Communication';
 import { useGlobalStore } from '~/stores';
 import type { GridCommunication } from '~/functions/types/GridCommunication';
 import { $fetch, FetchError } from 'ofetch';
+import { GridToolOptions } from '~/functions/types/GridTool';
 
 const store = useGlobalStore();
 const isWaitingForData: Ref<boolean> = ref(false);
@@ -95,6 +96,29 @@ const popoverMessage = computed(() => {
   return 'Compute grid placement for every grid';
 });
 
+const addAdditionalDataToPrimaryLine = (primaryLine: GridToolOptions.compactPrimaryLine): GridToolOptions.primaryLine => {
+  return {
+    active: false,
+    color: undefined,
+    handles: {
+      points: primaryLine.points.map((point: GridToolOptions.compactGridPoint): GridToolOptions.gridPoint => {
+        return {
+          x: point.x,
+          y: point.y,
+          isCommonPoint: point.isCommonPoint,
+          highlight: false,
+          active: false,
+          lines: [],
+        }
+      }),
+    },
+    invalidated: false,
+    uuid: primaryLine.uuid,
+    visible: true,
+  };
+
+};
+
 const computeGrids = async (): Promise<void> => {
   store.animation.isComputingGrids = true;
   const comm = new Communication();
@@ -108,7 +132,9 @@ const computeGrids = async (): Promise<void> => {
 
     for (const grid of responseData.grids) {
       store.gridState?.tool.setStateForImageIds([], [grid.imageId]);
-      store.gridState?.tool.setStateForImageIds(grid.primaryLines, [grid.imageId], grid.includesRefinementPoints);
+
+      const primaryLines = grid.primaryLines.map((primaryLine) => addAdditionalDataToPrimaryLine(primaryLine));
+      store.gridState?.tool.setStateForImageIds(primaryLines, [grid.imageId], grid.includesRefinementPoints);
     }
   } catch (e) {
     if (!(e instanceof FetchError)) {
