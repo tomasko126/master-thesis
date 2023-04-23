7b77d7a8ba3fb932dff8bbf402bee589fa96e58c
Tomáš Taro <tomasko126@gmail.com>
2023-04-23 17:31:13 +0200

    Move app entrypoint logic to index.vue file (#45)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,20 @@
 <template>
-  <TheApp />
+  <main>
+    <TheTopPanel class="top-panel" />
+    <TheLeftPanel class="left-panel" />
+    <TheMainWindow class="main-window" />
+    <TheRightPanel class="right-panel" />
+  </main>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { initLibraries } from '~/functions/Cornerstone';
+
+onMounted(async () => {
+  await initLibraries();
+});
+</script>
 
 <style lang="scss">
 body {
@@ -9,5 +23,32 @@ body {
   color: rgba(255, 255, 255, 0.8);
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Helvetica Neue", sans-serif;
   font-weight: bold;
+}
+</style>
+
+<style lang="scss" scoped>
+main {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+
+  .top-panel {
+    grid-column: 1 / 6;
+    grid-row: 1;
+  }
+
+  .left-panel {
+    grid-column: 1;
+    grid-row: 2 / 4;
+  }
+
+  .main-window {
+    grid-column: 2 / 5;
+    grid-row: 2 / 4;
+  }
+
+  .right-panel {
+    grid-column: 5;
+    grid-row: 2 / 4;
+  }
 }
 </style>
