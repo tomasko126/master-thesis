d0280c38e11b4e955a5bc5739b8a8466a9b7d5a1
Tomáš Taro <tomasko126@gmail.com>
2023-04-24 23:28:40 +0200

    Remove unused packages (#46)
    * Remove unused packages
    
    * Update ESLint TS parser

--- app/plugins/ui.ts
+++ app/plugins/ui.ts
@@ -4,16 +4,14 @@ import { library, config } from '@fortawesome/fontawesome-svg-core';
 // eslint-disable-next-line import/named
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
 
 import { createVuestic } from 'vuestic-ui'
 import 'vuestic-ui/css';
-import '@fontsource/source-sans-pro';
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
-library.add(far, fas);
+library.add(fas);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon);
